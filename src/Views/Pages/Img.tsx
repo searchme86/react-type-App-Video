31fceed799a7584fcfab04282e117e2dc9c594f7
searchme86 <searchme86@gmail.com>
2022-02-26 23:58:34 +0900

    ⚡️ imgUploader value null cleared

--- src/Views/Pages/Img.tsx
+++ src/Views/Pages/Img.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import '../Assets/Styles/Img.css';
 import ImageBox from '../Components/ImgUploadFunc/ImageBox';
 
 function ImgSlider() {
+  const InputRef = useRef<HTMLInputElement>(null);
+  const [imgList, setImgList] = useState<string[]>([]);
+
+  const clickBtn = () => {
+    InputRef.current?.click();
+  };
+  const checkItem = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.currentTarget.value) {
+      setImgList((prev) => [...prev, event.currentTarget?.value]);
+    }
+  };
+
+  console.log('img-list', imgList);
+
   return (
     <div className="container">
       <div className="initial-box">
@@ -11,8 +25,11 @@ function ImgSlider() {
           <br />
           이미지를 추가해주세요
         </div>
-        <div className="plus-box">+</div>
+        <div className="plus-box" onClick={clickBtn}>
+          +
+        </div>
       </div>
+      <input type="file" ref={InputRef} onChange={checkItem} />
       <div className="image-holder">
         <ImageBox
           src="https://media.vlpt.us/images/cjy0029/post/a67c40fa-bfb2-4aea-b9ba-440a3994518c/reactquery.png"
